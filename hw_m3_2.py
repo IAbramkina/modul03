0372e91b30d9793bf664b4dd0d54a29e2698fef7
IAbramkina <sinbellezza@gmail.com>
2024-07-15 10:10:45 +0200

    Update hw_m3_2.py

--- hw_m3_2.py
+++ hw_m3_2.py
@@ -8,7 +8,8 @@ def get_numbers_ticket(min, max, quantity):
         return []
     if min >=max:
         return []
-
+    if (max - min) < quantity:
+        return []
     return sorted(sample(range(min, max), quantity))
         
-print("Ваші лотерейні числа:", get_numbers_ticket(1, 10, 5))
+print("Ваші лотерейні числа:", get_numbers_ticket(10, 20, 11))
